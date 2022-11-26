130b86688dfb60811fbc4dcbe5d6612aec32bfb5
William Atkin <w.j.atkin-2021@hull.ac.uk>
2022-11-26 13:17:52 +0000

    Initial aim vector seems to work, needs more work though.

--- ThreeThingGame/Assets/PlayerInput/BallShooter.cs
+++ ThreeThingGame/Assets/PlayerInput/BallShooter.cs
@@ -14,25 +14,13 @@ public class BallShooter : MonoBehaviour
     public Rigidbody rBody;
     public float Force;
 
+    public LineRenderer AimLine;
+
     private DragPhase phase;
 
-    private void Update()
+    private bool TryResolveMousePoint(out Vector3 point)
     {
         var cam = Camera.main;
-
-        if (phase == DragPhase.None)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                phase = DragPhase.Dragging;
-            }
-        }
-
-        if(phase == DragPhase.Dragging)
-        {
-
-        }
-
         var camRay = cam.ScreenPointToRay(Input.mousePosition);
 
         var plane = new Plane(Vector3.up, Vector3.zero);
@@ -44,27 +32,98 @@ public class BallShooter : MonoBehaviour
             var srcPoint = transform.position;
             srcPoint.y = hitPoint.y;
 
-            var dir = (hitPoint - srcPoint).normalized;
-            Debug.DrawLine(srcPoint, hitPoint);
+            point = hitPoint;
+            return true;
+        }
 
-            switch (phase)
-            {
-                case DragPhase.None:
-                    {
+        point = default;
+        return false;
+    }
 
-                    }
-                    break;
-                case DragPhase.Dragging:
-                    {
+    private Vector3 ResolveAimVector(Vector3 source, Vector3 point)
+    {
+        var dir = point - source;
+        var aimDir = -dir;
 
-                    }
-                    break;
-            }
+        var mod = 1.0f;
+        return aimDir * mod;
+    }
+
+    private Vector3 ResolveAimLineEndpoint(Vector3 aimVector)
+    {
+        return transform.position + aimVector;
+    }
+
+    private void Update()
+    {
+        if (phase == DragPhase.None)
+        {
+            AimLine.gameObject.SetActive(false);
 
             if (Input.GetMouseButtonDown(0))
             {
-                rBody.AddForce(dir * Force, ForceMode.Impulse);
+                phase = DragPhase.Dragging;
             }
         }
+
+        if (phase == DragPhase.Dragging)
+        {
+            AimLine.gameObject.SetActive(true);
+
+            if (!TryResolveMousePoint(out Vector3 hitPoint))
+            {
+                Debug.Log("didnt resolve");
+                return;
+            }
+
+            var aimVector = ResolveAimVector(transform.position, hitPoint);
+            var end = ResolveAimLineEndpoint(aimVector);
+
+            AimLine.SetPosition(0, transform.position);
+            AimLine.SetPosition(1, end);
+
+            if (!Input.GetMouseButton(0))
+            {
+                var force = aimVector;
+                rBody.AddForce(force, ForceMode.Impulse);
+
+                phase = DragPhase.None;
+            }
+        }
+
+        //var cam = Camera.main;
+        //var camRay = cam.ScreenPointToRay(Input.mousePosition);
+
+        //var plane = new Plane(Vector3.up, Vector3.zero);
+        //if (plane.Raycast(camRay, out float enter))
+        //{
+        //    var hitPoint = camRay.GetPoint(enter);
+
+        //    // Match the Y level
+        //    var srcPoint = transform.position;
+        //    srcPoint.y = hitPoint.y;
+
+        //    var dir = (hitPoint - srcPoint).normalized;
+        //    Debug.DrawLine(srcPoint, hitPoint);
+
+        //    switch (phase)
+        //    {
+        //        case DragPhase.None:
+        //            {
+
+        //            }
+        //            break;
+        //        case DragPhase.Dragging:
+        //            {
+
+        //            }
+        //            break;
+        //    }
+
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        rBody.AddForce(dir * Force, ForceMode.Impulse);
+        //    }
+        //}
     }
 }
