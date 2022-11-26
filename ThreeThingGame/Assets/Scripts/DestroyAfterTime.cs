735d5d22767228a68a76c1f381427cbcb2ed2c04
Connor Kerwin <connorkerwin@yahoo.co.uk>
2022-11-26 22:57:31 +0000

    Polished some of the destruction, added a destructive locomotive, added another cabin basis (prefab only)

--- ThreeThingGame/Assets/Scripts/DestroyAfterTime.cs
+++ ThreeThingGame/Assets/Scripts/DestroyAfterTime.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DestroyAfterTime : MonoBehaviour
 {
     public float DestructionTime = 1.0f;
 
+    public UnityEvent OnBeforeDestroy;
+
     public void BeginDestructionCountdown()
     {
         Destroy(gameObject, DestructionTime);
+    }
+
+    private void OnDestroy()
+    {
+        OnBeforeDestroy?.Invoke();
     }
 }
