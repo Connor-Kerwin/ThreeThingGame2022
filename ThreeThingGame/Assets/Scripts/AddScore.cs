300acc8d8339b012595c7c6837cc1983e607f661
Connor Kerwin <connorkerwin@yahoo.co.uk>
2022-11-26 22:32:06 +0000

    Further destruction behaviour. Added a shell for score interaction (not implemented)

---
+++ ThreeThingGame/Assets/Scripts/AddScore.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    public int ScoreToAdd;
+
+    public void AddSomeScore()
+    {
+        // TODO: Score should be added here...
+    }
+}
